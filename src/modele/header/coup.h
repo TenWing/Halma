f41c2601708b9ae4f7f621d7298e50679718aa07
RAKOTONDRAMASY tendry <rakotondramasy@e204pc12.ecole.ensicaen.fr>
2014-11-21 14:11:49 +0100

    Ajout de nouveaux elements

---
+++ src/modele/header/coup.h
@@ -0,0 +1,57 @@
+/**
+ * \file      coup.h
+ * \author    Tendry
+ * \version   1.0
+ * \date      21 novembre 2014
+ * \brief     Définit la structure d'un coup, mais aussi d'une liste de coups
+ *
+ * \details   Un coup est considéré comme étant un déplacement de pion, dans le jeu de l'Halma, un pion peut se déplacer
+ 			  plusieurs fois dans un tour en sautant d'autre pions et en enchaînant les sauts, chaque étape intermédiaire
+ 			  est un coup.
+ 			  Une liste de coups est considérée comme une pile de coups, puisque lorsqu'on dépile le dernier l'élément
+ 			  le plus haut de la pile est sensé être l'avant dernier coup.
+*/
+
+#ifndef COUP_H
+#define COUP_H
+
+//######################################
+//Inclusions
+#include "pion.h"
+#include "position.h"
+//######################################
+
+/**
+* \brief Un coup peut se résumer en un déplacement d'un pion
+* \author Tendry
+* \version 1.0
+*/
+typedef (struct Coup, Coup);
+struct Coup
+{
+	/**
+	* \brief le pion qui se déplace
+	* \details Un pointeur car il s'agit d'une information ne nécessitant pas d'exister en tant que tel
+	* \see Pion
+	*/
+	Pion* pion;
+
+	/**
+	* \brief la position que le pion occupait juste avant la fin de ce coup
+	* \see Position
+	*/
+	Position precedente;
+};
+
+/**
+* \brief Une liste de coups contient plusieurs coups sous forme de file
+* \author Tendry
+* \version 1.0
+*/
+typedef(struct Coups, Coups);
+struct Coups
+{
+	//TODO
+};
+
+#endif
---
+++ src/modele/header/tour.h
@@ -0,0 +1,47 @@
+/**
+* \file		tour.h
+* \author	Tendry
+* \date		21 Novembre 2014
+* \version	1.0
+* \brief	Définition de la structure d'un tour de jeu
+* \details	Un tour de jeu consiste en une pile de coups, un pion originel que le joueur déplace, ainsi que sa position de départ
+			absolue avant TOUT coup.
+*/
+
+#ifndef TOUR_H
+#define TOUR_H
+
+//######################################
+//Inclusions
+#include "pion.h"
+#include "position.h"
+//######################################
+
+/**
+* \brief Un tour de jeu dans la partie
+* \author Tendry
+* \version 1.0
+*/
+typedef(struct Tour, Tour);
+struct Tour
+{
+	/**
+	* \brief Le pion concerné par le tour 
+	* \see Pion
+	*/
+	Pion pion;
+
+	/**
+	* \brief la pile de coups jouée dans ce tour
+	* \see Coups
+	*/
+	Coups coups;
+
+	/**
+	* \brief la position de départ du pion concerné au début du tour
+	* \see position
+	*/
+	Position depart;
+};
+
+#endif

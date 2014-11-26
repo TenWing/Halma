556a856c4b1db84b3f6dde42911b40e554078fe4
RAKOTONDRAMASY tendry <rakotondramasy@e207pc04.ecole.ensicaen.fr>
2014-11-26 15:39:10 +0100

    MAKEFILES MAAAAJ

--- src/modele/header/tour.h
+++ src/modele/header/tour.h
@@ -5,7 +5,7 @@
 * \date		21 Novembre 2014
 * \version	1.0
 * \brief	Définition de la structure d'un tour de jeu
-* \details	Un tour de jeu consiste en une pile de coups, un pion originel que le joueur déplace, ainsi que sa position de départ
+* \details	Un tour de jeu consiste en une pile de tours, un pion originel que le joueur déplace, ainsi que sa position de départ
 			absolue avant TOUT coup.
 */
 
@@ -33,10 +33,10 @@ struct Tour
 	Pion pion;
 
 	/**
-	* \brief la pile de coups jouée dans ce tour
-	* \see Coups
+	* \brief la pile de tours jouée dans ce tour
+	* \see tours
 	*/
-	Coups coups;
+	tours tours;
 
 	/**
 	* \brief la position de départ du pion concerné au début du tour
@@ -44,5 +44,86 @@ struct Tour
 	*/
 	Position depart;
 };
+
+/**
+* \brief un noeud d'une pile de tours
+* \author Tendry
+* \version 1.0
+*/
+typedef struct NoeudTour NoeudTour;
+struct NoeudTour
+{
+	/**
+	* \brief le tour, valeur du noeud
+	* \see Tour
+	*/
+	Tour tour;
+
+	/**
+	* \brief un pointeur vers le noeud suivant
+	*/
+	NoeudTour* suivant;
+};
+
+/**
+* \brief Une pile de tours
+* \author Tendry
+* \version 1.0
+* \details la pile de tours contient plusieurs noeud contenant les tours
+*/
+typedef struct PileTours PileTours;
+struct PileTours
+{
+	/**
+	* \brief le premier noeud de la pile
+	* \details structure de données de type pile
+	* \see NoeudTour
+	*/
+	NoeudTour* premier;
+};
+
+//######################################
+//FONCTIONS TOUR
+//######################################
+
+/**
+* \brief initialise un tour
+* \author Tendry
+* \version 1.0
+* \param p le pion qui concerne le tour
+* \return renvoie un Tour bien initialisé
+*/
+Tour tour_init(Pion p);
+
+//######################################
+//FONCTIONS PILE TOURS
+//######################################
+
+/**
+* \brief initialise une pile de tours
+* \author Tendry
+* \version 1.0
+* \return Une pile de tourss initialisée
+*/
+PileTours pileTours_init();
+
+/**
+* \brief dépile le dernier élément des tours
+* \author Tendry
+* \version 1.0
+* \param pile la pile de tours
+* \return Le coup en haut de la pile
+*/
+Coup pileTours_depiler(PileTours* pile);
+
+/**
+* \brief ajoute un coup en haut de la pile
+* \author Tendry
+* \version 1.0
+* \param pile la pile de tours
+* \param coup le coup à ajouter
+*/
+void pileTours_ajouterCoup(PileTours* pile, Coup coup);
+
 
 #endif

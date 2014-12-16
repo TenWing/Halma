915b14dbca4d6f07f5b6f136748072dae35c8e55
RAKOTONDRAMASY tendry <rakotondramasy@e207pc04.ecole.ensicaen.fr>
2014-12-16 17:53:09 +0100

    preparation IA et nettoyage .o

---
+++ src/modele/header/intelligence.h
@@ -0,0 +1,16 @@
+/**
+* \file		intelligence.h
+* \author	Tendry
+* \brief	
+* \version	1.0
+*/
+
+#ifndef INTELLIGENCE_H
+#define	INTELLIGENCE_H
+
+// #####################
+// INLCUSIONS
+#include <direction.h>
+// #####################
+
+#endif
